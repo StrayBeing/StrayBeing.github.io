ef9c43daacc9fd0b545bfb9f3c614cb4e1a502c4
unknown <>
2024-11-14 17:59:54 +0100

    hosting prep

--- SobierajskiMaciejLabJS/mathOperations1.js
+++
@@ -1,9 +0,0 @@
-const myMathModule1 = {
-    add: (a, b) => a + b,
-    subtract: (a, b) => a - b,
-    multiply: (a, b) => a * b,
-    divide: (a, b) => b !== 0 ? a / b : "Nie można dzielić przez zero",
-    power: (a, b) => Math.pow(a, b),
-  };
-  
-  module.exports = myMathModule1;
